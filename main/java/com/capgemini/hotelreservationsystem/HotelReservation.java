f27e15fde68c2db964fd0253f9fe332122f9f9b2
unknown <rachitshukla08@gmail.com>
2020-10-05 18:38:32 +0530

    Added: UC1 Ability to add a Hotel in Hotel Reservation System

---
+++ main/java/com/capgemini/hotelreservationsystem/HotelReservation.java
@@ -0,0 +1,35 @@
+package com.capgemini.hotelreservationsystem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class HotelReservation 
+{
+	private List<Hotel> hotelList = new ArrayList<Hotel>();
+	
+	public boolean addHotel(String hotelName, int regularCustRate) {
+		Hotel hotel = new Hotel(hotelName,regularCustRate);
+		hotelList.add(hotel);
+		return true;
+	}
+	
+    public static void main( String[] args )
+    {
+    	Scanner sc = new Scanner(System.in);
+    	HotelReservation hotelReservation = new HotelReservation();
+    	
+        System.out.println( "Welcome to Hotel Reservation System Program" );
+        System.out.println("Add a hotel \nEnter hotel name:");
+        String hotelName = sc.nextLine();
+        System.out.println("Enter regular customer rate:");
+        int regularCustRate = Integer.parseInt(sc.nextLine());
+        hotelReservation.addHotel(hotelName,regularCustRate);
+    }
+	
+	
+}
---
+++ test/java/com/capgemini/hotelreservationsystem/HotelTest.java
@@ -0,0 +1,19 @@
+package com.capgemini.hotelreservationsystem;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HotelTest 
+{
+    @Test
+    public void whenHotelAdded_ShouldReturnTrue()
+    {	
+    	HotelReservation hotelReservation = new HotelReservation();
+    	boolean isHotelAdded = hotelReservation.addHotel("Lakewood",110);
+        assertTrue(isHotelAdded);
+    }
+}
