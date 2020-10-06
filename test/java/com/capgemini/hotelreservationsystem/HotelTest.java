b1e213449fb6041f95cd8d3933a9bf033233ce40
unknown <rachitshukla08@gmail.com>
2020-10-06 11:46:05 +0530

    Refactored: Find cheapest hotel method to return cheapest hotel based on weekday or weekend

--- test/java/com/capgemini/hotelreservationsystem/HotelTest.java
+++ test/java/com/capgemini/hotelreservationsystem/HotelTest.java
@@ -1,7 +1,9 @@
 package com.capgemini.hotelreservationsystem;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -9,11 +11,22 @@ import org.junit.Test;
  */
 public class HotelTest 
 {
+	HotelReservation hotelReservation;
+	@Before
+	public void init() {
+		 hotelReservation = new HotelReservation();
+	}
     @Test
-    public void whenHotelAdded_ShouldReturnTrue()
-    {	
-    	HotelReservation hotelReservation = new HotelReservation();
+    public void whenHotelAdded_ShouldReturnTrue() {
     	boolean isHotelAdded = hotelReservation.addHotel("Lakewood",110, 90);
         assertTrue(isHotelAdded);
+    }
+    @Test 
+    public void whenGivenDate_OneToSixOct2020_ShouldReturnLakewoodHotelCheapest() {
+        hotelReservation.addHotel("Lakewood", 110, 90);
+        hotelReservation.addHotel("Bridgewood", 160, 60);
+        hotelReservation.addHotel("Ridgewood", 220, 150);
+    	Hotel hotel = hotelReservation.findCheapestHotel("01-Oct-2020", "06-Oct-2020");
+    	assertEquals("Lakewood", hotel.getHotelName());
     }
 }
